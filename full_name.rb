c51fb2aa9934f2c6240a4f09a3b8539b4b1e6b49
cdbbinary <cdb.binary@gmail.com>
2018-07-17 11:10:38 -0400

    letters.rb

--- full_name.rb
+++ full_name.rb
@@ -1,15 +1,22 @@
-puts 'Enter your first name? '
-first_name = gets.chomp
-puts 'Enter your middle name? '
-middle_hame = gets.chomp
-puts 'Enter your last name? '
-last_name = gets.chomp
-full_name = first_name + ' ' + middle_hame + ' ' + last_name
-puts 'Hello ' + full_name
-name_chars = first_name + middle_name + last_name
-name_chars = name_chars.length.to_s
-puts 'Did you know that your name has ' + name_chars + 'characters in it?'
+# puts 'Enter your first name? '
+# first_name = gets.chomp
+# puts 'Enter your middle name? '
+# middle_name = gets.chomp
+# puts 'Enter your last name? '
+# last_name = gets.chomp
+# full_name = first_name + ' ' + middle_name + ' ' + last_name
+# #full_name = [first_name, ' ', middle_name, ' ', last_name]
+# puts 'Hello ' + full_name
 
+# Experimenting with the 'join' method to refactored program.
+# My was intended to remove the spaces in the full_name variable.
+# full_name = full_name.join.length
+
+# name_chars = first_name + middle_name + last_name
+# name_chars = name_chars.length.to_s
+# puts 'Did you know that your name has ' + name_chars + ' characters in it?'
+
+# ******************************
 # puts 'What is your full name?'
 # name = gets.chomp
 # puts 'Did you know there are ' + name.length.to_s + ' characters'
