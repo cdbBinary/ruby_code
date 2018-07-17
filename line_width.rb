27edcf8a6024af13a9dabd73c55a0b90ed9664bd
cdbbinary <cdb.binary@gmail.com>
2018-07-17 12:02:16 -0400

    line_justify.rb

--- line_width.rb
+++ line_width.rb
@@ -4,3 +4,4 @@ puts('Sat in her cupboard'.center(line_width))
 puts('Eating her curds and whey'.center(line_width))
 puts('When along came a spider'.center(line_width))
 puts('And scared her poor shoe dog away'.center(line_width))
+
