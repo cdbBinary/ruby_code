75187ed307a57e45d488514b84d2f2551bd185e7
cdbbinary <cdb.binary@gmail.com>
2018-07-17 13:37:49 -0400

    angry_boss.rb

---
+++ angry_boss.rb
@@ -0,0 +1,9 @@
+puts 'Hey you what do you want from me already, can\'t you see I\'m working over here?'
+answer = gets.chomp
+puts 'WHADDAYA MEAN "I WANT A RAISE"?!? YOU\'RE FIRED!!'
+fired_1 = gets.chomp
+puts 'WHADDAYA MEAN "I WANT A RAISE"?!? YOU\'RE FIRED!!'
+fired_2 = gets.chomp
+puts 'WHADDAYA MEAN "I WANT A RAISE"?!? YOU\'RE FIRED!!'
+fired_3 = gets.chomp
+puts 'WHADDAYA MEAN "I WANT A RAISE"?!? YOU\'RE FIRED!!'
