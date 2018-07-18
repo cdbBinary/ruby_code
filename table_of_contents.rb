af9b35300694e774a28e6c5b089c02143cfe751b
cdbbinary <cdb.binary@gmail.com>
2018-07-17 22:01:55 -0400

    'table_of_contents.rb'

---
+++ table_of_contents.rb
@@ -0,0 +1,14 @@
+
+puts ''
+line_width = 40
+str_1 = 'Chapter 1: Getting Started'
+str_2 = 'page 1'
+str_3 = 'Chapter 2: Numbers'
+str_4 = 'Page 9'
+str_5 = 'Chapter 3: Letters'
+str_6 = 'Page 13'
+puts('Table of Contents'.center(line_width))
+puts ''
+puts(str_1.ljust(line_width) + str_2.rjust(line_width/2))
+puts(str_3.ljust(line_width) + str_4.rjust(line_width/2))
+puts(str_5.ljust(line_width) + str_6.rjust(line_width/2))
