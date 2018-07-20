5dfaaf2022d5e5dbdd92472e939b05d757458caf
cdbbinary <cdb.binary@gmail.com>
2018-07-19 23:26:45 -0400

    fixed while loop

--- 99_bottles.rb
+++ 99_bottles.rb
@@ -4,22 +4,23 @@
 # selection = gets.chomp
 # one = selection.to_i
 number = 100
-
 # if one == 1
 	
-while number > 99 do
+while number > 1 do
 
-	puts "#{number} bottles of beer on the wall, 99 bottles of beer.
-				Take one down and pass it around, 98 bottles of beer on the wall."
+	number = number -= 1
+	
+	less_one = number - 1
 
-	nummber = number -= 1
+	puts "#{number} bottles of beer on the wall, #{number} bottles of beer. Take one down and pass it around, #{less_one} bottles of beer on the wall."
+	puts ' '
 
-	if number == 1
-		puts "1 bottle of beer on the wall, 1 bottle of beer.
+	if less_one == 1
+		puts "#{number} bottle of beer on the wall, 1 bottle of beer.
 					Take one down and pass it around, no more bottles of beer on the wall."
 	end
 
-	if number == 0
+	if less_one == 0
 		puts "No more bottles of beer on the wall, no more bottles of beer. 
 					Go to the store and buy some more, 99 bottles of beer on the wall."
 	end
