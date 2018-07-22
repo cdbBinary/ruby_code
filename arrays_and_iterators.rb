f8a344bc21cf626537ddeefa04037abeb74d5c04
cdbbinary <cdb.binary@gmail.com>
2018-07-22 15:05:58 -0400

    more array methods

--- arrays_and_iterators.rb
+++ arrays_and_iterators.rb
@@ -1,7 +1,52 @@
-[]
-[5]
-['Hello', 'Goodbye']
+# []
+# [5]
+# ['Hello', 'Goodbye']
 
-flavor = 'vanilla'
-[89.9, flavor, [true, false]]
+# flavor = 'vanilla'
+# [89.9, flavor, [true, false]]
 
+# ****************************
+# names = %w[Ada Belle Chris]
+
+# puts names
+# puts
+# puts names[0]
+# puts names[1]
+# puts names[2]
+# puts names[3]
+
+# ****************************
+# other_peeps = []
+
+# other_peeps[3] = 'beebee Meager'
+# other_peeps[0] = 'Ah-ha'
+# other_peeps[1] = 'Seedee'
+# other_peeps[0] = 'beebee Ah-ha'
+
+# puts other_peeps 
+
+# *****************************
+# languages = %w[English Norweigan Ruby]
+
+# languages.each do |lang|
+# 	puts 'I love ' + lang + '!'
+# 	puts 'Don\'t you?'
+# end
+
+# puts 'And let\'s hear it for Java!'
+# puts '<crickets chirp in the distance>'
+
+# ***************************************
+foods = ['artichoke', 'brioche', 'caramel']
+
+puts foods
+puts
+puts foods.to_s
+puts
+puts foods.join(', ')
+puts
+puts foods.join(' :) ') + ' 8)'
+
+200.times do
+	puts []
+end
