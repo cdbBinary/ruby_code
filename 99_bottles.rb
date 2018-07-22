8474a367012537efaf668b26a0cd3db99a801add
cdbbinary <cdb.binary@gmail.com>
2018-07-21 23:57:47 -0400

    arrays and interators and 99 bottles edit

--- 99_bottles.rb
+++ 99_bottles.rb
@@ -11,12 +11,12 @@ while number > 0 do
 	puts ' '
 
 	if less_one == 1
-		puts "#{number} bottle of beer on the wall, #{number} bottle of beer. Take one down and pass it around, #{number} bottles of beer on the wall."
+		puts "#{number} bottle of beer on the wall, \n#{number} bottle of beer. \nTake one down and pass it around, \n#{number} bottles of beer on the wall."
 		puts ' '
 	end
 
 	if number == 0
-		puts "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
+		puts "No more bottles of beer on the wall, \nno more bottles of beer. \nGo to the store and buy some more, \n99 bottles of beer on the wall."
 		puts ' '
 	end
 end
