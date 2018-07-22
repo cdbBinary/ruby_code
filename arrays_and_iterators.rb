22bc31826adb73852955373c4afdae8487e95fee
cdbbinary <cdb.binary@gmail.com>
2018-07-22 15:30:21 -0400

    favorites array

--- arrays_and_iterators.rb
+++ arrays_and_iterators.rb
@@ -37,16 +37,30 @@
 # puts '<crickets chirp in the distance>'
 
 # ***************************************
-foods = ['artichoke', 'brioche', 'caramel']
+# foods = ['artichoke', 'brioche', 'caramel']
 
-puts foods
-puts
-puts foods.to_s
-puts
-puts foods.join(', ')
-puts
-puts foods.join(' :) ') + ' 8)'
+# puts foods
+# puts
+# puts foods.to_s
+# puts
+# puts foods.join(', ')
+# puts
+# puts foods.join(' :) ') + ' 8)'
 
-200.times do
-	puts []
-end
+# 200.times do
+# 	puts []
+# end
+
+# **************************************
+favorites = []
+favorites.push 'raindrops on raoses'
+favorites.push 'whiskey on kittens'
+
+puts favorites[0]
+puts favorites.last
+puts favorites.length
+
+puts favorites.pop
+puts favorites
+puts favorites.length
+
